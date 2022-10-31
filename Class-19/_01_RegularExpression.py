089189d6d6e1a9c8768c404d808d3c04147f4fad
Hasan Mahmud Rhidoy <hasan.mahmud@bracits.com>
2022-10-31 09:50:34 +0600

    Updated course content

--- Class-19/_01_RegularExpression.py
+++ Class-19/_01_RegularExpression.py
@@ -1,8 +1,6 @@
 """
 A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
-
 RegEx can be used to check if a string contains the specified search pattern.
-
 """
 
 # RegEx Functions
