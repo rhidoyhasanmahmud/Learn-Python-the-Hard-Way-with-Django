4f842cc2b53ac17dae9460fa232e8c6ace243500
Hasan Mahmud Rhidoy <hasan.mahmud@bracits.com>
2022-12-18 14:04:07 +0600

    File, Excel, Error Handling

---
+++ Class-19/_02_Error_Handling.py
@@ -0,0 +1,15 @@
+try:
+  # Dangerous stuff
+except ValueError:
+  # If you use try, at least 1 except block is mandatory!
+  # Handle it somehow / ignore
+except (BadThingError, HorrbileThingError) as e:
+  # Hande it differently
+except:
+  # This will catch every exception.
+else:
+  # Else block is not mandatory.
+  # Dangerous stuff ended with no exception
+finally:
+  # Finally block is not mandatory.
+  # This will ALWAYS happen after the above blocks.
